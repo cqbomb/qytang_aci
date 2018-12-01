46e3a0757dfd0fa80f0ed8d760596d5d844510cd
Administrator <collinsctk@qytang.com>
2018-12-01 14:54:19 +0800

    inital commit

---
+++ bin/wsdump.py
@@ -0,0 +1,4 @@
+#!/root/ACI/bin/python
+# EASY-INSTALL-SCRIPT: 'websocket-client==0.54.0','wsdump.py'
+__requires__ = 'websocket-client==0.54.0'
+__import__('pkg_resources').run_script('websocket-client==0.54.0', 'wsdump.py')
